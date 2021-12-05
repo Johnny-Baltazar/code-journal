24c7caa2a7a7ce7095b65b173cf7988711313dfe
Johnny Baltazar <baltazarjohnny@yahoo.com>
2021-12-04 22:28:51 -0800

    Listen for 'submit' events on the journal entry form. #1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,2 @@
 /* global data */
 /* exported data */
-var $input = document.querySelector('.url-input');
-var $img = document.querySelector('.image-update');
-
-$input.addEventListener('input', inputUrl);
-
-function inputUrl(event) {
-  $img.setAttribute('src', $input.value);
-}
