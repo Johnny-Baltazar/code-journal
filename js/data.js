21d0424b4928a09c3038f33fb7f822553483e63c
Johnny Baltazar <baltazarjohnny@yahoo.com>
2021-12-05 12:13:08 -0800

    Verify that each new entry receives an incrementing entryId.

--- js/data.js
+++ js/data.js
@@ -6,35 +6,3 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
-
-var $input = document.querySelector('.url-input');
-var $img = document.querySelector('.image-update');
-var $form = document.querySelector('#form');
-
-$input.addEventListener('input', inputUrl);
-$form.addEventListener('submit', handleSubmit);
-
-function inputUrl(event) {
-  $img.setAttribute('src', $input.value);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-
-  var title = $form.elements.title.value;
-  var url = $form.elements.url.value;
-  var notes = $form.elements.notes.value;
-  var formInputs = {
-    title: title,
-    url: url,
-    notes: notes
-  };
-
-  formInputs.nextEntryId = data.nextEntryId;
-  formInputs.nextEntryId++;
-
-  data.entries.unshift(formInputs);
-
-  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
-  $form.reset();
-}
--- js/main.js
+++ js/main.js
@@ -1,2 +1,41 @@
 /* global data */
 /* exported data */
+
+var $input = document.querySelector('.url-input');
+var $img = document.querySelector('.image-update');
+var $form = document.querySelector('#form');
+var formInputs = {};
+
+$input.addEventListener('input', inputUrl);
+$form.addEventListener('submit', handleSubmit);
+
+function inputUrl(event) {
+  $img.setAttribute('src', $input.value);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  var title = $form.elements.title.value;
+  var url = $form.elements.url.value;
+  var notes = $form.elements.notes.value;
+
+  formInputs = {
+    title: title,
+    url: url,
+    notes: notes
+  };
+
+  formInputs.nextEntryId = data.nextEntryId++;
+
+  data.entries.unshift(formInputs);
+
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+
+  this.localStorage.setItem('data-model', dataJSON);
+});
